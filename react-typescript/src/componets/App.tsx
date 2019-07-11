b1edfb0350f62e0f58ed1f2d62fc644247abc4f6
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-07-11 15:08:22 +0300

    start of project

--- react-typescript/src/componets/App.tsx
+++ react-typescript/src/componets/App.tsx
@@ -1,13 +1,23 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState } from 'react';
 //styles
 import './App.module.css';
 
+type FormElement = React.FormEvent<HTMLFormElement>
+
 export default function App(): JSX.Element{
+  const [value, setValue] = useState<string>('')
+  
+  
+  const handleSubmit = (e: FormElement):void => {
+    e.preventDefault();
+    setValue('')
+  }
+
   return (
     <Fragment>
       <h1>To Do List</h1>
-      <form>
-        <input type="text" required></input>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)}required></input>
         <button type="submit">Add ToDo</button>
       </form>
     </Fragment>
