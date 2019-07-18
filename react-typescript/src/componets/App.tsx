919cb859ca006a26c60df17b22eb99a637fcc35b
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-07-18 23:02:30 +0300

    const [todos, setTodos] = useState<ITodo[]>([])

--- react-typescript/src/componets/App.tsx
+++ react-typescript/src/componets/App.tsx
@@ -1,27 +1,40 @@
-import React, {Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 //styles
 import './App.module.css';
 
-type FormElement = React.FormEvent<HTMLFormElement>
+type FormElement = React.FormEvent<HTMLFormElement>;
 
-export default function App(): JSX.Element{
-  const [value, setValue] = useState<string>('')
-  
-  
-  const handleSubmit = (e: FormElement):void => {
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElement): void => {
     e.preventDefault();
-    setValue('')
-  }
+    addTodo(value);
+    setValue('');
+  };
 
+  const addTodo = (text: string) => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
   return (
     <Fragment>
       <h1>To Do List</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)}required></input>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
         <button type="submit">Add ToDo</button>
       </form>
     </Fragment>
   );
 }
-
-
